83ba637d6ed7ec532ab280e9aa8bd7475ebd4f02
Zeeshan Ali Khan <zeenix@gmail.com>
2024-03-26 16:45:06 +0100

    Impl a simple dummy service to start with

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+use tower::limit::ConcurrencyLimitLayer;
+use tower_http::trace::TraceLayer;
+use tracing::info;
+
+async fn handle() -> impl IntoResponse {
+    "OK"
 }
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let middleware = tower::ServiceBuilder::new()
+        .layer(TraceLayer::new_for_http())
+        .layer(ConcurrencyLimitLayer::new(1));
+
+    let app = Router::new()
+        .route("/api/v1", get(handle))
+        .layer(middleware);
+
+    let listener = tokio::net::TcpListener::bind(ADDRESS).await.unwrap();
+    info!("listening on {ADDRESS}");
+    axum::serve(listener, app).await.unwrap();
+}
+
+const ADDRESS: &str = "localhost:23818";
